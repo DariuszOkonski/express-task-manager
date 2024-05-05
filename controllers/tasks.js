2fecaa335571ca40ccfca0206d7c6cd5655165bf
Dariusz Okoński <darek200180@gmail.com>
2024-05-05 15:28:53 +0200

    add trycatch at createTask

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -5,9 +5,12 @@ const getAllTasks = (req, res) => {
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    return res.status(201).json({ task });
+  } catch (error) {
+    return res.status(500).json({ msg: error });
+  }
 };
 
 const getTask = (req, res) => {
